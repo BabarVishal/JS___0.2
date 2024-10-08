68e22c8711b975fd8ed2f4f5669e05393a1f9c1c
vishal <vishalbabar7351@gmail.com>
2024-10-08 19:47:44 +0530

    Data Types

---
+++ 03.DataType/script.js
@@ -0,0 +1,27 @@
+//Number...
+let number = 22;
+let numTwo = 22;
+
+console.log(numTwo + numTwo);
+
+//String
+let myName = "Vishal";
+console.log(myName);
+
+//Booleans
+let isTrue = true;
+
+//null
+let lastLoginDate = null;
+
+//objects
+
+let person = {
+   fisthName: "Vishal",
+   lastName: "Babar",
+   age: 22
+}
+
+console.log(person);
+console.log(person.fisthName);
+
