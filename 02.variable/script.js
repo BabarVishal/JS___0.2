abb11d6b101c744953b9aee702cdf550e6caf73f
vishal <vishalbabar7351@gmail.com>
2024-10-08 19:11:04 +0530

    first commit

---
+++ 02.variable/script.js
@@ -0,0 +1,25 @@
+console.log("Hello Vishal");
+// var, let, const
+
+// var  ----> Global 
+
+if(true){
+    var age = 22;
+}
+
+console.log(age);
+
+//let ----> Local
+
+if(true){
+    let age = 23;
+    console.log(age);
+}
+
+//const ---> Local,  Not re-assin the value!
+
+
+
+
+
+
